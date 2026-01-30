4f39d15b29b3d4e432579fde4544659b158d32c6
Chulosmiranha <chulosmiranha@gmail.com>
2026-01-30 18:44:53 -0300

    Primeiro commit

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -1,35 +1,23 @@
+import { DoorClosed, DoorOpen } from 'lucide-react-native';
 import { Tabs } from 'expo-router';
-import React from 'react';
-
-import { HapticTab } from '@/components/haptic-tab';
-import { IconSymbol } from '@/components/ui/icon-symbol';
-import { Colors } from '@/constants/theme';
-import { useColorScheme } from '@/hooks/use-color-scheme';
 
 export default function TabLayout() {
-  const colorScheme = useColorScheme();
-
   return (
     <Tabs
-      screenOptions={{
-        tabBarActiveTintColor: Colors[colorScheme ?? 'light'].tint,
-        headerShown: false,
-        tabBarButton: HapticTab,
-      }}>
-      <Tabs.Screen
-        name="index"
-        options={{
-          title: 'Home',
-          tabBarIcon: ({ color }) => <IconSymbol size={28} name="house.fill" color={color} />,
-        }}
-      />
-      <Tabs.Screen
-        name="explore"
-        options={{
-          title: 'Explore',
-          tabBarIcon: ({ color }) => <IconSymbol size={28} name="paperplane.fill" color={color} />,
-        }}
-      />
+        screenOptions={{
+            tabBarActiveTintColor: 'gray',
+            headerStyle: {
+                backgroundColor: 'black',
+            },
+            headerShadowVisible: false,
+            headerTintColor: 'white',
+            tabBarStyle: {
+              backgroundColor: 'black',
+            },
+        }}>
+      <Tabs.Screen name="index" options={{ title: 'Home', 
+        tabBarIcon: ({focused}) => (focused ? <DoorOpen color="#E8E8E8" size="20" /> : <DoorClosed color="#E8E8E8" size="20" />) 
+        }}/>
     </Tabs>
   );
 }
---
+++ app/+not-found.tsx
@@ -0,0 +1,38 @@
+import { SafeAreaView } from 'react-native-safe-area-context';
+import { StyleSheet, Text } from 'react-native';
+import { Link, Stack } from 'expo-router';
+
+export default function NotFoundScreen() {
+  return (
+    <>
+      <Stack.Screen options={{ title: 'My dad' }} />
+      <SafeAreaView style={styles.container}>
+        <Text style={styles.text}> Looks like you found my dad a place that can't be found </Text>
+        <Link href="./" style={styles.button}>
+          Go back to Home screen!
+        </Link>
+      </SafeAreaView>
+    </>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#25292e',
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+
+  button: {
+    fontSize: 20,
+    textDecorationLine: 'underline',
+    color: '#fff',
+  },
+
+  text: {
+    fontSize: 30,
+    color: '#fff',
+    fontWeight: '500',
+  }
+});
